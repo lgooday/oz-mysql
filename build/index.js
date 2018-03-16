93d285fc069e6d8c004d99876f4732f12abcc402
lgooday <36068283+lgooday@users.noreply.github.com>
2018-03-16 10:04:01 +0100

    add built version

---
+++ build/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.close = exports.dqf = exports.dq = exports.init = undefined;
+
+var _promiseMysql = require('promise-mysql');
+
+var _promiseMysql2 = _interopRequireDefault(_promiseMysql);
+
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var pool = void 0;
+
+var init = function init(conf) {
+	pool = _promiseMysql2.default.createPool(conf);
+};
+
+var getConnection = function getConnection() {
+	return pool.getConnection().disposer(function (connection) {
+		console.log('disposing');
+		pool.releaseConnection(connection);
+	});
+};
+
+var dq = function dq(query, params, tag) {
+	// eslint-disable-line no-unused-vars
+	return _bluebird2.default.using(getConnection(), function (connection) {
+		return connection.query(query, params);
+	});
+};
+
+var dqf = function dqf(query, params, tag) {
+	// eslint-disable-line no-unused-vars
+	return _bluebird2.default.using(getConnection(), function (connection) {
+		return connection.query(query, params).then(function (rows) {
+			if (rows && rows.length === 1) {
+				return rows[0];
+			}
+
+			return _bluebird2.default.reject(new Error('not 1 item returned'));
+		});
+	});
+};
+
+var close = function close() {
+	return pool.end();
+};
+
+exports.init = init;
+exports.dq = dq;
+exports.dqf = dqf;
+exports.close = close;
